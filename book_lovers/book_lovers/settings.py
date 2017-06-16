8c57f78283169020429d9ba42b1222888c0524fc
rmoskow1 <rmoskowitz314@gmail.com>
2017-06-16 17:11:14 -0400

    Add the models!

--- book_lovers/book_lovers/settings.py
+++ book_lovers/book_lovers/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'my_books.apps.MyBooksConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

a80673437780d3578fed42133e8dfc96f5246b46
rmoskow1 <rmoskowitz314@gmail.com>
2017-06-18 12:32:25 -0400

    .as_view()
    The previous patterns didn't give anything to do at the given url -- it
    said, go to this view class (like view.BooksCreateView) but didn't say
    what to do. It needs to be a function, here the .as_view() function

--- book_lovers/my_books/urls.py
+++ book_lovers/my_books/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from my_books import views
 
 urlpatterns = [
-    url(r'^(?P<pk>\d+)/$', views.BooksDetailView, name='detail'),
-    url(r'^$', views.BooksCreateView, name='create'),
-    url(r'^(?P<pk>\d+)/$', views.BooksUpdateView, name='update'),
+    url(r'^(?P<pk>\d+)/$', views.BooksDetailView.as_view(), name='detail'),
+    url(r'^$', views.BooksCreateView.as_view(), name='create'),
+    url(r'^(?P<pk>\d+)/$', views.BooksUpdateView.as_view(), name='update'),
 ]

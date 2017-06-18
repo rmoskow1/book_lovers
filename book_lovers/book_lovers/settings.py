1e290bd99f888a92080156080a2c79ec44e33127
rmoskow1 <rmoskowitz314@gmail.com>
2017-06-18 15:47:46 -0400

    Restarted everything - no new User
    Earlier, everything failed because the user was changed - after previous
    migrations had already recognized a different user model...messy stuff.
    Now, there is no new user model - I changed this in the previous commit.
    I think the only difference now is that the pychache has all my new
    actions remembered, and doesn't know about that previous new kind of
    user.

--- book_lovers/book_lovers/settings.py
+++ book_lovers/book_lovers/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'my_books.apps.MyBooksConfig',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',

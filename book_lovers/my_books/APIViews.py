3f705babbc03751da23e004657e35cdaf7432b45
rmoskow1 <rmoskowitz314@gmail.com>
2017-06-30 12:27:32 -0400

    Change the counting of the  popularbooks set

--- book_lovers/my_books/APIViews.py
+++ book_lovers/my_books/APIViews.py
@@ -49,8 +49,9 @@ class BookViewSet(viewsets.ModelViewSet):
 
 #display only the books with at least 2 users who favorite
 class PopularBookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.filter(users_who_favorite__gte=1)
-  
+    #queryset = Book.objects.filter(users_who_favorite__gte=1).distinct()
+    num_fans = 1
+    queryset = Book.objects.annotate(users=Count('users_who_favorite')).filter(users__gte=num_fans) #filter out the books with num_fans users 
     serializer_class = BookSerializer
     
 
--- book_lovers/my_books/serializers.py
+++ book_lovers/my_books/serializers.py
@@ -32,7 +32,7 @@ class BookSerializer(serializers.ModelSerializer): #we'll be converting somethin
     
 class UserSerializer(serializers.ModelSerializer):
     #owned_books = serializers.PrimaryKeyRelatedField(many = True, queryset = Book.objects.all())
-    owned_books = BookSerializer(many = True) #Lists are not currently supported in HTML input. - from the form. Books are displayed in full. You have to enter data for books as full dictionaries, maybe creating a new one is fine?
+   # owned_books = BookSerializer(many = True) #Lists are not currently supported in HTML input. - from the form. Books are displayed in full. You have to enter data for books as full dictionaries, maybe creating a new one is fine?
 #without either, u can enter primary keys to books when creating a user, but can't create books at the same time
     class Meta:
         model = User

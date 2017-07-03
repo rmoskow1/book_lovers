ddcb532f2983b1b0c390f94e5226d0a168793930
rmoskow1 <rmoskowitz314@gmail.com>
2017-07-03 15:27:25 -0400

    Change author serializer fields
    now, all fields are specified in the class's Meta

--- book_lovers/my_books/serializers.py
+++ book_lovers/my_books/serializers.py
@@ -5,11 +5,10 @@ from django.db import models
 
 
 
-
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ('name',)
+        fields = '__all__'
 
 class BookSerializer(serializers.ModelSerializer): #we'll be converting something to JSON based on the model
     owner = models.ForeignKey('auth.User', related_name='owned_books',  on_delete = models.CASCADE)
@@ -18,7 +17,6 @@ class BookSerializer(serializers.ModelSerializer): #we'll be converting somethin
     
     class Meta:
         model = Book
-        #what attributes do we want to return to the user? Not necesarily everything...
         fields = ('id','title','author','publisher','owner','users_who_favorite')
         #everything: fields = '__all__'
     
@@ -33,10 +31,6 @@ class BookSerializer(serializers.ModelSerializer): #we'll be converting somethin
 class UserSerializer(serializers.ModelSerializer):
     #owned_books = serializers.PrimaryKeyRelatedField(many = True, queryset = Book.objects.all())
    # my_owned_books = BookSerializer(many = True, ) #Lists are not currently supported in HTML input. - from the form. Books are displayed in full. You have to enter data for books as full dictionaries, maybe creating a new one is fine?
-   # owned_books = BookSerializer(many = True, read_only=True)
-
-    #Lists are not currently supported in HTML input. - from the form. Books are displayed in full. You have to enter data for books as full dictionaries, maybe creating a new one is fine?
-#without either, u can enter primary keys to books when creating a user, but can't create books at the same time
     class Meta:
         model = User
         fields = ('username','email', 'owned_books', 'fav_books')

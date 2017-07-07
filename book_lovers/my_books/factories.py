537791616859efab38e41a83c9ed754ce022fb12
rmoskow1 <rmoskowitz314@gmail.com>
2017-07-07 15:18:46 -0400

    Revert "Revert "Fix errors in userFactory field names and book attribute names""
    This reverts commit 01a00d7bded031bc89965955d05014dc533481ad.

--- book_lovers/my_books/factories.py
+++ book_lovers/my_books/factories.py
@@ -2,7 +2,7 @@ from .models import Book, Publisher, Author
 from django.contrib.auth.models import User
 import factory
 import factory.fuzzy
-
+import datetime
 # factories.py
 
 class PublisherFactory(factory.DjangoModelFactory):
@@ -13,35 +13,8 @@ class PublisherFactory(factory.DjangoModelFactory):
     name = factory.fuzzy.FuzzyText()
     address = factory.fuzzy.FuzzyText()
     city = factory.fuzzy.FuzzyText()
-
-class BookFactory(factory.DjangoModelFactory):
-    class Meta:
-        model = Book
-
-    title = factory.fuzzy.FuzzyText()
-    publisher = factory.SubFactory(PublisherFactory) #if it's one to many
-    date = factory.fuzzy.FuzzyDate()
-    pen_name = factory.fuzzy.FuzzyText()
-    uploader = factory.SubFactory(UserFactory)
-    author = factory.SubFactory(UserFactory)
-    uploader= factor.SubFactory(UserFactory)
-    is_verified = False #default creation of a book
-    def is_public(self):
-        return False #this method is false by default
-   
-                
-    @factory.post_generation
-    def users_who_favorite(self, create, extracted, **kwargs):
-        if not create:
-            # Simple build, do nothing.
-            return
-
-        if extracted:
-            # A list of groups were passed in, use them
-            for user in extracted:
-                self.users_who_favorite.add(user) 
-                
-
+    
+    
     
 class UserFactory(factory.DjangoModelFactory):
     class Meta:
@@ -59,6 +32,38 @@ class UserFactory(factory.DjangoModelFactory):
         if not create:
             # Fiddle with django internals so self.product_set.all() works with build()
             self._prefetched_objects_cache = {'fav_book': fav_books}  
+
+class BookFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Book
+
+    title = factory.fuzzy.FuzzyText()
+    publisher = factory.SubFactory(PublisherFactory) #if it's one to many
+    date = factory.fuzzy.FuzzyDate(datetime.date(2008, 1, 1))
+    pen_name = factory.fuzzy.FuzzyText()
+    uploader = factory.SubFactory(UserFactory)
+    author = factory.SubFactory(UserFactory)
+    uploader= factory.SubFactory(UserFactory)
+    isPublished = False
+    isVerified = False #default creation of a book
+    def isPublic(self):
+        return False #this method is false by default
+   
+                
+    @factory.post_generation
+    def users_who_favorite(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of groups were passed in, use them
+            for user in extracted:
+                self.users_who_favorite.add(user) 
+                
+
+    
+
 
      
     

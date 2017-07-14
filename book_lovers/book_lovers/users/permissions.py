a84a17f5bcd4d8775c1c41078010659e31cd171f
rmoskow1 <rmoskowitz314@gmail.com>
2017-07-14 15:52:53 -0400

    Create User App
    Move user/profile related views and models to the user app

---
+++ book_lovers/book_lovers/users/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+
+# a permission designed for UserViewSet -- only staff can see a list of all users.
+# And only staff or the user itself can view a user detail page
+class IsStaffOrTargetUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        # allow user to list all users if logged in user is staff
+        return view.action == 'retrieve' or request.user.is_staff
+
+    def has_object_permission(self, request, view, obj):
+        # allow logged in user to view own details, allows staff to view all records
+        return request.user.is_staff or obj == request.user
